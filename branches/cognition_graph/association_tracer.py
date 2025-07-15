774804e6b600b063f8275ccec347d81d36fbb939
bendinglee <bennie08dingle@gmail.com>
2025-07-15 11:21:57 +0100

    Create association_tracer.py

---
+++ branches/cognition_graph/association_tracer.py
@@ -0,0 +1,5 @@
+def trace_associations():
+    return [
+        { "source": "memory", "target": "persona", "type": "influence" },
+        { "source": "persona", "target": "vision", "type": "styling" }
+    ]
