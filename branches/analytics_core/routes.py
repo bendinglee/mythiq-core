1de03dfbfd4248544336411b8edfd2397be6d2ba
bendinglee <bennie08dingle@gmail.com>
2025-07-15 13:56:01 +0100

    Update routes.py

--- branches/analytics_core/routes.py
+++ branches/analytics_core/routes.py
@@ -1,2 +1,8 @@
 from flask import Blueprint, jsonify
 from .trend_monitor import get_trend
+
+analytics_bp = Blueprint("analytics_bp", __name__)
+
+@analytics_bp.route("/trend", methods=["GET"])
+def trend_report():
+    return jsonify(get_trend())
