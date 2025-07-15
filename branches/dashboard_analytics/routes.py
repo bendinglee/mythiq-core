8f4886624f4f08670e489da2d6866acc601ca2c1
bendinglee <bennie08dingle@gmail.com>
2025-07-15 10:36:01 +0100

    Update routes.py

--- branches/dashboard_analytics/routes.py
+++ branches/dashboard_analytics/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify
 from .metrics_collector import collect_metrics
+
 dashboard_bp = Blueprint("dashboard_bp", __name__)
 
 @dashboard_bp.route("/status", methods=["GET"])
-def system_status():
+def dashboard_status():
     return jsonify(collect_metrics())
