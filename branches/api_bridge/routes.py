d0651b2adc882b2c6f66c44bd0b869573d7ec51a
bendinglee <bennie08dingle@gmail.com>
2025-07-16 12:42:26 +0100

    Update routes.py

--- branches/api_bridge/routes.py
+++ branches/api_bridge/routes.py
@@ -3,7 +3,7 @@ from .call_handler import fetch_data
 
 bridge_bp_internal = Blueprint("bridge_bp_internal", __name__)
 
-@bridge_bp.route("/fetch", methods=["POST"])
+@bridge_bp_internal.route("/fetch", methods=["POST"])
 def fetch():
     config = request.json
     return jsonify(fetch_data(config))
