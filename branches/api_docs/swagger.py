bf182d982d54f9d14180525d125e3915d1ced9d1
bendinglee <bennie08dingle@gmail.com>
2025-07-16 13:23:00 +0100

    Update swagger.py

--- branches/api_docs/swagger.py
+++ branches/api_docs/swagger.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify
 
 docs_bp_ui = Blueprint("docs_bp_ui", __name__)
 
-@docs_bp.route("/docs/swagger", methods=["GET"])
+@docs_bp_ui.route("/docs/swagger", methods=["GET"])
 def swagger_ui():
     return jsonify({
         "ui": "Swagger viewer endpoint",
