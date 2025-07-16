3932c9eab506232af7ff347c685e90aebfdd3c0c
bendinglee <bennie08dingle@gmail.com>
2025-07-16 12:30:42 +0100

    Update routes.py

--- branches/api_bridge/routes.py
+++ branches/api_bridge/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from .call_handler import fetch_data
 
-bridge_bp = Blueprint("bridge_bp", __name__)
+bridge_bp_internal = Blueprint("bridge_bp_internal", __name__)
 
 @bridge_bp.route("/fetch", methods=["POST"])
 def fetch():
